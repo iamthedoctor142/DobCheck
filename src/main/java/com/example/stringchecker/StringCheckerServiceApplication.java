f29c4bec3be22f3bc6ce94a099777f56c5a573cc
anil <anilcrj123@gmail.com>
2025-04-19 20:22:55 +0530

    initial commit

---
+++ src/main/java/com/example/stringchecker/StringCheckerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.stringchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringCheckerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringCheckerServiceApplication.class, args);
+	}
+
+}
